3f3dcf18027dc0c0b4efd47807c9fe0781465ad0
Bernhard Nickel <bernhard@Bernhards-MBP.home>
2015-04-07 23:04:22 +0200

    inital services

---
+++ rosebery-common/src/main/java/at/ac/tuwien/infosys/rosebery/common/service/publication/DefaultPublicationService.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.infosys.rosebery.common.service.publication;
+
+import java.io.Serializable;
+
+/**
+ * @author Bernhard Nickel, e0925384, e0925384@student.tuwien.ac.at
+ */
+public class DefaultPublicationService implements PublicationService {
+    @Override
+    public <T extends Serializable> void publish(T t) {
+
+    }
+}
