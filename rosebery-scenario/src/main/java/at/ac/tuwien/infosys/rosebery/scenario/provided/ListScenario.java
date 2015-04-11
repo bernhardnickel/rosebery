53fbd7de563b518b36f761a09915173a1215a84a
Bernhard Nickel <bernhard@218-162.swlan.wu-wien.ac.at>
2015-04-11 19:49:18 +0200

    dsl

---
+++ rosebery-scenario/src/main/java/at/ac/tuwien/infosys/rosebery/scenario/provided/ListScenario.java
@@ -0,0 +1,26 @@
+package at.ac.tuwien.infosys.rosebery.scenario.provided;
+
+import at.ac.tuwien.infosys.rosebery.scenario.Factory;
+import at.ac.tuwien.infosys.rosebery.scenario.PopulationService;
+import at.ac.tuwien.infosys.rosebery.scenario.Scenario;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Bernhard Nickel, e0925384, e0925384@student.tuwien.ac.at
+ */
+public class ListScenario<T> implements Scenario<T> {
+    private List<Scenario<T>> scenarios;
+
+    ListScenario(Scenario<T>... scenarios) {
+        this.scenarios = Arrays.asList(scenarios);
+    }
+
+    @Override
+    public void run(Factory<T> factory, PopulationService<T> populationService) {
+        for (Scenario<T> scenario : scenarios) {
+            scenario.run(factory, populationService);
+        }
+    }
+}
